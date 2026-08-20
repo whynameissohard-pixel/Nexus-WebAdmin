c2b4d4e38dcec68338a32ec64f4bf289b235bfa4
Sasiprapha Praphansiri <whansasiprapha@gmail.com>
2026-08-20 20:55:08 +0700

    Delete simulate_face_scan.js

--- simulate_face_scan.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const { v4: uuidv4 } = require('uuid');
-
-// Connect to the local emulator or actual project (assumes you have serviceAccountKey if needed outside functions, 
-// but since we just use it directly, we can rely on standard init if running via a local script configured for Firebase).
-// Actually, to make it easy for the user to run in the frontend context, we can just write a quick node script that imports their frontend firebase.js.
-
-// But wait, the frontend firebase.js is for browser. A node script would need firebase-admin.
-// Let's create a script that just runs a quick simulation via REST or just give instructions.
